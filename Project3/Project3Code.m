5f428fd09d4d647ef6b10424c0f2292fb57a4302
Keefe Kamp (El Yunque) <>
2023-02-06 18:31:57 -0500

    Updated Code

--- Project3/Project3Code.m
+++ Project3/Project3Code.m
@@ -17,7 +17,7 @@ imagesc(bCorr)
 colormap('gray')
 title('Correlation')
 axis off image
-
+exportgraphics(gcf,'Correlation.png','Resolution',300)
 figure
 
 cConv=conv2(points,ramp, 'same'); 
@@ -25,7 +25,9 @@ imagesc(cConv)
 colormap('gray')
 title('Convolution')
 axis off image
+exportgraphics(gcf,'Convolution.png','Resolution',300)
 
+%PArt2
 words=im2gray(imread("Text.png"));
 
 w=im2gray(imread('w.png'));
@@ -35,18 +37,19 @@ figure
 imagesc(textcorr)
 colormap('gray')
 axis off image
+title('Text Correlation')
+exportgraphics(gcf,'TextCorr.png','Resolution',300)
 
 NormFilter=ones(size(w));
 NormFilter=1/sum(NormFilter,'all') .* NormFilter;
-
 NormConv=conv2(textcorr,NormFilter,'same');
-
 Norm=textcorr./NormConv;
-
 figure
 imagesc(Norm)
 colormap('gray')
 axis off image
+title('Normailized Text Correlation')
+exportgraphics(gcf,'NormTextCorr.png','Resolution',300)
 %Part3
 
 a=imread('Drops.jpeg','jpeg'); %reading in image
@@ -56,32 +59,31 @@ noise=uint8(floor(randn(256).*20+20)); %making noise array
 b=a+noise; %adding noise to original image
 
 sobx=[1 0 -1;2 0 -2;1 0 -1];
-edges=filter2(sobx,b);
+edges=filter2(sobx,b,"valid");
 figure
 imagesc(edges)
 colormap('gray')
 axis off image
+title('Sobel x Edge Detection')
+exportgraphics(gcf,'SobelXDrops.png','Resolution',300)
 
-uni=ones(5);
+uni=ones(3);
 uni=1/sum(uni,'all') * uni;
-
 uniDrops=filter2(uni,b,'valid');
+uniDropsEdge=filter2(sobx,uniDrops,'valid');
 figure
-imagesc(uniDrops)
+imagesc(uniDropsEdge)
 colormap('gray')
 axis off image
+title('Sobel x Edge Detection prepossessed with Uniform Filter')
+exportgraphics(gcf,'UniSobelXDrops.png','Resolution',300)
 
-x1 = -2:1:2;
-x2 = -2:1:2;
-[X1,X2] = meshgrid(x1,x2);
-X = [X1(:) X2(:)];
-
-y = mvnpdf(X,3,7);
-y = reshape(y,length(x2),length(x1));
-
-GaussDrops=filter2(y,b,'valid');
+Gauss=1/16 .* [1 2 1;2 4 2;1 2 1];
+GaussDrops=filter2(Gauss,b,'valid');
+GaussDropsEdge=filter2(sobx,GaussDrops,'valid');
 figure
-imagesc(GaussDrops)
+imagesc(GaussDropsEdge)
 colormap('gray')
 axis off image
-
+title('Sobel x Edge Detection prepossessed with Gausian Filter')
+exportgraphics(gcf,'GaussSobelXDrops.png','Resolution',300)
