67423272f2bb3ab2963fe0f9273f55352af08536
Keefe Kamp <kkamp2015@my.fit.edu>
2023-03-03 02:10:32 -0500

    Fixed filter

--- Project5/AdaptFilter.m
+++ Project5/AdaptFilter.m
@@ -1,7 +1,6 @@
-function A=AdaptFilter(X)
-    eta=100;
+function A=AdaptFilter(X,eta)
     ml=mean(X,'all');
-    L=var(cast(X,'double'),0,"all")^2;
+    L=var(cast(X,'double'),0,"all");
     g=X(ceil(length(X)/2),ceil(length(X)/2));
-    A=g-(((eta/L)*g)-ml);
+    A=g-((eta/L)*(g-ml));
 end
