d67b5233a9de42da3f6a913613c0dde5dd567b6e
kkamp2015 <kkamp2015@my.fit.edu>
2023-04-18 12:15:07 -0400

    Project Code attempt 1

--- Project10/Project10Code.m
+++ Project10/Project10Code.m
@@ -5,5 +5,30 @@ close all
 Cells=imread('Cells.tif');
 Cells=im2gray(Cells);
 
-minval=min(Cells);
-Maxval=max(Cells);
+minval=double(min(Cells,[],'all'));
+maxval=double(max(Cells,[],'all'));
+
+hist=histogram(Cells,256);
+Mg=mean(Cells,'all');
+histnorm=hist.Values/sum(hist.Values,'all');
+P1=zeros([1,255]);
+M1=zeros([1,255]);
+M2=zeros([1,255]);
+for i = minval:maxval
+    P1(i)=sum(histnorm(1:i),"all");
+    t=Cells<i;
+    M1(i)=mean(Cells(t));
+    M2(i)=mean(Cells(~t));
+
+end
+P2=ones(size(P1))-P1;
+sigB2=(Mg.*P1-M1.*P1).^2 ./(P1.*P2);
+
+[TreshVal,Tresh]=max(sigB2);
+
+CellsTresh=uint8(Cells<Tresh+1);
+figure
+imagesc(CellsTresh)
+axis off image
+colormap('gray')
+
